e46698b00d8d66916cd9a523a5f56c71f2f6e2a3
charbel-ai <a01799433@tec.mx>
2023-09-03 20:29:17 -0600

    Organizacion del codigo

--- retobuluk.cpp
+++ retobuluk.cpp
@@ -1,23 +1,30 @@
-// Este espacio se usara solo para el codigo AUTOMATA
 #include <iostream>
 #include <string>
 
 // Cada serie de accion imprimira: que hizo, en cuanto tiempo lo realizo cada accion y cuanto tiempo se tardo en total
 // Funciones para cada serie de accion
 
+int tiempototal; 
 //Esta serie de accion dura 9 segundos - esto se mostrar al momento de dar las opciones
 //La serie de accion A serie de accion se centrara en que el autónomo avanze 3 metros
 //Se detenga y despues mgire hacia la direccion donde comenzo para asi volver al lugar donde comenzo
 void seriedeaccionA (){
+std:: cout<<"El autonomo avanzo tres metros, 3 segundos"<<std::endl; 
+std:: cout<<"El autonomo se detuvo y gire 180 grados hacia la derecha, volteando a la direccion donde empezo, 4 segundos"<<std::endl; 
+std:: cout<<"El autonomo avanzo tres metros volviendo al lugar donde empezo, 3 segundos"<<std::endl; 
+std:: cout<<"El tiempo total de accion fue de: " << tiempototal <<std::endl; 
 
 
+int tiempototal = 10; 
 }
+
 
 //Esta serie de accion dura 10 segundos
 //La serie de accion B hara que el autónomo avanza 4 metros, se detenga, gire su propio eje volviendo a la misma posicion donde estaba
 // Despues el autonomo retrodecera 2 metros y avanzara 4 metros.
 void seriedeaccionB (){
 
+int tiempototal = 10; 
 }
 
 //Esta serie de accion dura 15 segundos
@@ -27,6 +34,7 @@ void seriedeaccionB (){
 //Avanzara 5 metros volviendo al lugar donde empezo 
 void seriedeaccionC (){
 
+int tiempototal = 15; 
 }
 
 
@@ -64,5 +72,10 @@ else if (seriedeaccion == 'C'){
     void seriedeaccionC();
 
 }
+
+//Inicio del programa teleoperado
+std::cout<<"Ingrese T para pasar al programa teleoperado"<<std::endl; 
+
+
 
 }
