aad957ce5aae46ac0d65d2e1463166dcd7658463
charbel-ai <a01799433@tec.mx>
2023-09-08 22:35:14 -0600

    Cambios en los botones (botones para pistones)

--- retobuluk.cpp
+++ retobuluk.cpp
@@ -10,6 +10,7 @@ int tiempolimite = 147;
 int tiempo;
 int grados; 
 std::string direccion;  
+char decision; 
 char seriedeaccion;
 char seriedeaccion2 = std::toupper(seriedeaccion);
 char boton;
@@ -19,8 +20,17 @@ char boton;
 // Este boton esta conectado al puerto CAN del roborio 
 void botonA (){
   std::cout<<"Boton A: Control del movimiento del autonomo (adelante,atras,izquierda,derecha)"<<std::endl; 
+   
+   //Codigo que se repite hasta que el usuario ingrese una direccion valida 
+   do{
   std::cout<<"Ingrese la direccion: "<<std::endl; 
   std::cin>>direccion; 
+  if( (direccion != "izquierda" ) && (direccion != "derecha" ) && (direccion != "atras" ) && (direccion != "adelante" )){
+    std::cout<<"Ingrese una direccion que exista"<<std::endl; 
+  }
+   }
+   while ((direccion != "izquierda" ) && (direccion != "derecha" ) && (direccion != "atras" ) && (direccion != "adelante" )); 
+
   std::cout<<"Ingrese el tiempo en segundos: "<<std::endl; 
   std::cin>>tiempo; 
   std::cout<<""<<std::endl; 
@@ -32,8 +42,17 @@ void botonA (){
 //Esta conectado al puerto CAN del roborio
 void botonB(){
   std::cout<<"Boton B: Controla el giro del autonomo"<<std::endl; 
+ 
+  //Codigo que se repite hasta que el usuario ingrese una direccion valida 
+   do{
   std::cout<<"Ingrese la direccion: "<<std::endl; 
   std::cin>>direccion; 
+  if( (direccion != "izquierda" ) && (direccion != "derecha" ) && (direccion != "atras" ) && (direccion != "adelante" )){
+    std::cout<<"Ingrese una direccion que exista"<<std::endl; 
+  }
+   }
+   while ((direccion != "izquierda" ) && (direccion != "derecha" ) && (direccion != "atras" ) && (direccion != "adelante" )); 
+
   std::cout<<"Ingrese el tiempo en segundos: "<<std::endl; 
   std::cin>>tiempo; 
   std::cout<<"Ingrese los grados que gire el autonomo: "<<std::endl; 
@@ -41,6 +60,23 @@ void botonB(){
   std::cout<<""<<std::endl; 
   std::cout<<"El autonomo a girado hacia la "<<  direccion << " " << grados << " grados, por "<< tiempo << " segundos"<<std::endl; 
    
+}
+
+void botonC(){
+  std::cout<<"El boton C: Estira los pistones y toma una llanta"<<std::endl; 
+  std::cout<<"Los pistones se han estirado"<<std::endl; 
+  std::cout<<"Has tomado una llanta"<<std::endl; 
+  std::cout<<"El tiempo de duracion fue de: 1 segundos  "<<std::endl; 
+  std::cout<<"Escoge el boton D para contraer los pistones y colocar la llanta en el elevador "<<std::endl;
+  tiempo = 1; 
+}
+void botonD(){
+  std::cout<<"El boton D: Contrae los pistones y coloca la llanta en el elevador "<<std::endl; 
+  std::cout<<"Los pistones se han contraido : "<<std::endl; 
+  std::cout<<"Has colocado una llanta en el elevador"<<std::endl; 
+  std::cout<<"El tiempo de duracion fue de: 3 segundo  "<<std::endl; 
+  tiempo = 3; 
+ 
 }
 
 
@@ -68,6 +104,7 @@ std:: cin>> seriedeaccion;
 if (seriedeaccion2 == 'A'){
 
 tiempototal = 10; 
+std:: cout<<"------ Serie de accion A ------"<<std::endl; 
 std:: cout<<"El robot avanzo tres metros, tardo: 3 segundos"<<std::endl; 
 std:: cout<<"El robot se detuvo y gire 180 grados, tardo: 4 segundos"<<std::endl; 
 std:: cout<<"El robot avanzo tres metros volviendo al lugar donde empezo, tardo: 3 segundos"<<std::endl; 
@@ -80,6 +117,7 @@ break;
 // Despues el autonomo retrodecera 2 metros y avanzara 4 metros.
 else if (seriedeaccion2 == 'B'){
 tiempototal = 13; 
+std:: cout<<"------ Serie de accion B ------"<<std::endl; 
 std:: cout<<"El robot avanzo cuatro metros, tardo: 4 segundos"<<std::endl; 
 std:: cout<<"El robot se detuvo y gire 180 grados, tardo: 3 segundos"<<std::endl; 
 std:: cout<<"El robot retrocedio 2 metros, tardo: 2 segundos"<<std::endl; 
@@ -96,15 +134,16 @@ break;
 // Al estar a la altura, los pistones se vuelven a estirar y pegan la llanta al coche con el velcro
 else if (seriedeaccion2 == 'C'){
 tiempototal = 15; 
+std:: cout<<"------ Serie de accion C ------"<<std::endl; 
 std:: cout<<"El robot ha identificado la posicion de la llanta y se ha colocado frente a ella, tardo: 1 segundos"<<std::endl; 
 std:: cout<<"Los pistones se han estirado, pegandose a la llanta,tardo: 2 segundos"<<std::endl; 
 std:: cout<<"Los pistones se han contraido y han dejado la llanta en posicion con el elevador, tardo: 3 segundos"<<std::endl; 
 std:: cout<<"El robot ha indentificado el coche y se ha puesto en posicion a este, tardo: 2 segundos"<<std::endl; 
-std:: cout<<"El elevador ha empezado a funcionar, a eleado la llanta a la altura del coche, tardo: 3 segundos"<<std::endl; 
-std:: cout<<"Los pistones se han estirado y pegado la llanta al coche, tardo: 3 segundos"<<std::endl; 
+std:: cout<<"El elevador ha empezado a funcionar, a elevado la llanta a la altura del coche, tardo: 3 segundos"<<std::endl; 
+std:: cout<<"Los pistones se han estirado y pegado la llanta al coche usando el velcro q ue tienen, tardo: 3 segundos"<<std::endl; 
 std:: cout<<"Los pistones se han contraido, tardo: 1 segundos"<<std::endl; 
 std:: cout<<"El tiempo total de accion fue de: "<< tiempototal<< " segundos"<<std::endl; 
-std:: cout<<"El tiempo de autonomo fue terminado completamente"<<std::endl; 
+std:: cout<<"El tiempo de autonomo ha sido completado con los 15 segundos dados"<<std::endl; 
 break; 
 }
 
@@ -120,19 +159,20 @@ while ((seriedeaccion2 != 'B') && (seriedeaccion2!= 'A') && (seriedeaccion2 != '
 //Inicio del programa teleoperado
 std::cout<<""<<std::endl; 
 std::cout<<"El programa autonomo ha terminado"<<std::endl; 
-std::cout<<"Inicion del programa teleoperado"<<std::endl; 
-
+std::cout<<""<<std::endl; 
+std::cout<<"------ Inicio del programa teleoperado ------"<<std::endl; 
 std:: cout<< " "<<std::endl; 
 
 //Menu de botones 
-std::cout<<"Exiten dos botones"<<std::endl; 
-std::cout<<"El boton A: Controla el avanze del robot (adelante,atras,izquierda,derecha)"<<std::endl; 
-std::cout<<"El boton B: Controla el giro del robot"<<std::endl; 
-std::cout<<""<<std::endl;
- 
- 
 //Esta parte del codigo se repetira hasta que el tiempo de teleoperado sea rebasado o se complete
  while(tiempototal <= tiempolimite ){
+
+std::cout<<"------ BOTONES ------"<<std::endl; 
+std::cout<<"El boton A: Controla el avanze del robot (adelante,atras,izquierda,derecha)"<<std::endl; 
+std::cout<<"El boton B: Controla el giro del robot"<<std::endl; 
+std::cout<<"El boton C: Estira los pistones"<<std::endl; 
+std::cout<<"El boton D: Contrae los pistones"<<std::endl; 
+std::cout<<""<<std::endl;
 std::cout<<"Elige un boton"<<std::endl;
 std::cin>> boton; 
 char boton2 = std::toupper(boton);
@@ -143,8 +183,19 @@ if (boton2 == 'A'){
 
 else if (boton2 == 'B'){
  botonB();
+
 }
-else if ('B' != boton2 != 'A'){
+
+else if (boton2 == 'C'){
+  botonC();
+}
+
+else if (boton2 == 'D'){
+  botonD(); 
+
+}
+
+if ((boton2 != 'A') && (boton2 != 'B' && (boton2 != 'C') && (boton2 != 'D'))){
   std::cout<< "Ingrese un boton que exista"<<std::endl; 
 }
 
@@ -163,14 +214,6 @@ if (tiempototal > 147){
 }
 
  
- 
-
-
-
-
-
-//-Acomodar el programa para que una parte quite y ponga las llantas
-//Cuando quede cierto tiempo, mostrar que acciones realizar, segun recomendaciones de scouting
 
 
 
